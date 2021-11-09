2f3428b3af19d1259039ecdede45773cce7039df
Vladislav Titov <dc.titovvlad@gmail.com>
2021-11-10 00:14:45 +0300

    Done MVVM

---
+++ flutter_mvvm/lib/view_model/list_picture_viewmodel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_mvvm/model/picsum_model.dart';
+import 'package:flutter_mvvm/services/service.dart';
+
+class ListPictureViewModel{
+  List<PictureViewModel>? pictures;
+
+  Future<void> fetchPictures() async{
+    final pictureResult = await Service().fetchPicturesApi(); 
+    pictures = pictureResult.map((e) => PictureViewModel(e)).toList();
+  }
+}
+
+class PictureViewModel {
+  final PicSumModel? picSumModel;
+
+  PictureViewModel(this.picSumModel);
+}
