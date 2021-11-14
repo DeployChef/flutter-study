28b32de5c1dcf6a759ead47fab82bd2845e4acb9
Vladislav Titov <dc.titovvlad@gmail.com>
2021-11-14 19:14:33 +0300

    planets app start

---
+++ flutter_planets/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_planets/ui/home/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Planets",
+    home: HomePage(),
+  ));
+}
