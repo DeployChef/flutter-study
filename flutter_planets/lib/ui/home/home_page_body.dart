00ae88bc1e2968e023df8fba788e42756d8829b3
Vladislav Titov <dc.titovvlad@gmail.com>
2021-11-15 23:01:38 +0300

    add list

--- flutter_planets/lib/ui/home/home_page_body.dart
+++ flutter_planets/lib/ui/home/home_page_body.dart
@@ -1,11 +1,32 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_planets/model/planet.dart';
 import 'package:flutter_planets/ui/home/planet_row.dart';
 
 class HomePageBody extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return PlanetRow(planets[0]);
+    return new Expanded(
+      child: new Container(
+        color: new Color(0xFF736AB7),
+        child: new CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: <Widget>[
+            new SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              sliver: new SliverFixedExtentList(
+                itemExtent: 161.0,
+                delegate: new SliverChildBuilderDelegate(
+                    (context, index) => new PlanetRow(planets[index]),
+                  childCount: planets.length,
+
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
